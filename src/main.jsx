0e3419d7f3c3173f5ba0d05f9eef39fdfc0ddf3a
Sean Currlin <sean.currlin@gmail.com>
2024-10-11 07:54:47 -0400

    install tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const main = () => {
+  return (
+    <div>main</div>
+  )
+}
+
+export default main
