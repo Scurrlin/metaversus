b11a3ebd8ac16299f0f9886b56827b4771019ea7
Sean Currlin <sean.currlin@gmail.com>
2024-10-12 17:27:24 -0400

    update Feedback.jsx content

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -21,17 +21,15 @@ const Feedback = () => (
         <div className="feedback-gradient" />
         <div>
           <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
-            Samantha
+            Adrian
           </h4>
           <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
-            Founder Metaverus
+            Founder, Metaverus
           </p>
         </div>
 
         <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
-          “With the development of today's technology, metaverse is very
-          useful for today's work, or can be called web 3.0. by using
-          metaverse you can use it as anything”
+        “As technology evolves, the Metaverse is transforming how we work, play, and connect. Metaversus offers limitless opportunities for innovation and exploration—allowing you to shape your own digital future.”
         </p>
       </motion.div>
 
